9e980a5c5663dbb110c451f87ec35f4103b2fa48
javidev <jajavieralonso9@gmail.com>
2025-04-22 21:39:16 +0200

    Change title

--- src/index.js
+++ src/index.js
@@ -4,4 +4,6 @@ const dado = document.querySelector("button");
 dado.addEventListener("click", function(){
     const number = 1 + Math.floor(Math.random() * 6);
     dado.textContent = number;
+
+    dado.classList.toggle("rotate");
 });
